2ad045f937b2985bbd1f4a2589fc2a78f3f78c14
lujan <lujan_exogenesis@hotmail.com>
2021-03-23 03:16:51 -0500

    RUP

---
+++ theme/appResource.js
@@ -0,0 +1,71 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2000, 2007 IBM Corporation.  All Rights Reserved.
+//------------------------------------------------------------------------------
+// this file define the localizable resource entries 
+// the strings defined here should be localized
+
+AppResource = function(){
+
+// translate the strings below
+
+// these are labels of the drawers
+this.drawer_searchResult="Resultado de la búsqueda";
+this.drawer_glossary="Glosario";
+this.drawer_index="Índice";
+this.drawer_profiling="Perfilado";
+
+this.dojoTreePane_error_load_tree="error al cargar árbol";
+
+// the icon tooltips in the toolbar
+this.toolbar_tooltip_display_dropdown_menu="Mostrar el menú desplegable";
+this.toolbar_tooltip_display_glossary="Visualizar glosario";
+this.toolbar_tooltip_display_index="Visualizar índice";
+this.toolbar_tooltip_sync="Sincroniza el árbol de vista con la página de contenido actual";
+this.toolbar_tooltip_email="Enviar comentarios";
+this.toolbar_tooltip_info="Mostrar información de versión y copyright";
+
+// the drop down menus
+this.toolbar_menu_showall="Mostrar todo";
+this.toolbar_menu_closeall="Cerrar todo";
+this.toolbar_menu_glossary="Glosario";
+this.toolbar_menu_index="Índice";
+this.toolbar_menu_search="Buscar";
+this.toolbar_menu_sync="Enlazar vista con página de contenido";
+this.toolbar_menu_feedback="Información de retorno";
+this.toolbar_menu_about="Acerca de";
+
+// search scope text
+this.searchScope_title="Ámbito de la búsqueda";
+this.searchScope_type_text="Tipo de página que se debe buscar:";
+this.searchScope_rpp="Resultados página:";
+this.searchScope_mc="Contenido del método";
+this.searchScope_role="Rol";
+this.searchScope_task="Tarea";
+this.searchScope_wp="Producto de trabajo";
+this.searchScope_guidance="Instrucciones";
+this.searchScope_process="Proceso";
+this.searchScope_activity="Actividad";
+this.searchScope_general="Contenido general";
+
+// search widget
+this.searchWidget_inprogress="Búsqueda en curso...";
+this.searchWidget_errorMessage = "Problema al llamar al motor de búsqueda";
+
+// the pop up window title for the about box
+this.about_box_title="Rational Method Composer";
+
+// feedback email subject text
+this.feedback_email_subject="Acerca del contenido: ";
+
+// end of strings 
+// ==================== no translation below this point ===================================
+	this.searchResultPaneLabel = this.drawer_searchResult;
+	this.glossaryPaneLabel =  this.drawer_glossary ;
+	this.indexPaneLabel = this.drawer_index ;
+	this.profilingPaneLabel = this.drawer_profiling;
+	
+	
+};
+
+
+
---
+++ theme/appSettings.js
@@ -0,0 +1,12 @@
+theApp.settings.enableProfiling=false
+theApp.settings.enableBookmark=true
+theApp.settings.autoSyncContentWithView=true
+theApp.settings.enableSearch=true
+theApp.settings.searchServlet=null
+theApp.settings.showIndex=true
+theApp.settings.showGlossary=true
+theApp.settings.showBanner=true
+theApp.feedbackURL="mailto:rupfeed@us.ibm.com?subject=\"Feedback: RUP7.0.1 on RMC 7.2 (Classic RUP for SOMA - es)\""
+theApp.configurationTitle="Classic RUP for SOMA.es"
+theApp.infoFileName="about.htm"
+theApp.bannerFileName="rup_banner.jpg"
